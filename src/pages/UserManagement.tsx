67d9732665f9a3ea59ae0e7dbc26428b39858774
aabhyahhh <abhayatrivedi2005@gmail.com>
2025-06-26 22:54:20 +0530

    error fixed

--- src/pages/UserManagement.tsx
+++ src/pages/UserManagement.tsx
@@ -172,19 +172,6 @@ export default function UserManagement() {
     });
   };
 
-  const dayStringToNumber = (day: string) => {
-    switch (day) {
-      case 'Sunday': return 0;
-      case 'Monday': return 1;
-      case 'Tuesday': return 2;
-      case 'Wednesday': return 3;
-      case 'Thursday': return 4;
-      case 'Friday': return 5;
-      case 'Saturday': return 6;
-      default: return -1;
-    }
-  };
-
   const handleAddUserSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setAddError(null);
